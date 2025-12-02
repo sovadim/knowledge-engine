e31f4d0ac20b466e0486dc3f6d77a469bb1a19b8
Vadim Sofin <Vadim_Sofin@epam.com>
2025-12-02 11:13:36 +0400

    feat: add dto classes

---
+++ backend/dto.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class NodeStatus(str, Enum):
+    PASSED = "passed"
+    FAILED = "failed"
+    IN_PROGRESS = "in_progress"
+    NOT_REACHED = "not_reached"
+    DISABLED = "disabled"
+
+
+class NodeLevel(str, Enum):
+    A1 = "A1"
+    A2 = "A2"
+    A3 = "A3"
+
+
+class Node(BaseModel):
+    id: int
+    status: NodeStatus
+    level: NodeLevel
+
+    child_nodes: List[int]
+    parent_nodes: List[int]
+
+    questions: Optional[List[str]] = None
+    criterias: Optional[List[str]] = None
